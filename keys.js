ab96e5c91f989fdc136e13a9b379c86ca162535e
Sean Rudd <seanmrudd@me.com>
2019-05-14 22:04:05 -0400

    api keys added

--- keys.js
+++ keys.js
@@ -4,3 +4,6 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+//OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=538eb1db
+//Spotify Client ID: 5a819eae38a54f7ab6c96118c4d50d37
