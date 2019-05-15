f41eb290bd755559f78c1bf73c539acb2e066368
shrudder <seanmrudd@me.com>
2019-05-15 12:51:11 -0400

    spotify api working

--- keys.js
+++ keys.js
@@ -5,5 +5,6 @@ exports.spotify = {
   secret: process.env.SPOTIFY_SECRET
 };
 
-//OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=538eb1db
-//Spotify Client ID: 5a819eae38a54f7ab6c96118c4d50d37
+// exports.omdb = {
+//   api: apikey
+// }
--- liri.js
+++ liri.js
@@ -1,3 +1,17 @@
 require("dotenv").config();
+
 var keys = require("./keys.js");
+
+var Spotify = require('node-spotify-api');
+
 var spotify = new Spotify(keys.spotify);
+
+//This works -- don't touch
+spotify
+  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+  .then(function(data) {
+    console.log(Object.keys(data)); 
+  })
+  .catch(function(err) {
+    console.error('Error occurred: ' + err); 
+  });
